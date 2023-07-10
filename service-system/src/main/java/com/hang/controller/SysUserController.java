761d4a43b91209918cd34d2a58e384e9a4b6054e
Yikoutian1 <599882460@qq.com>
2023-07-10 14:49:03 +0800

    添加前端页面

--- service-system/src/main/java/com/hang/controller/SysUserController.java
+++ service-system/src/main/java/com/hang/controller/SysUserController.java
@@ -1,5 +1,8 @@
 package com.hang.controller;
 
+import com.hang.service.SysUserService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  */
 
+@Api(tags = "用户管理接口")
 @RestController
-@RequestMapping("/sys-user")
+@RequestMapping("/admin/system/sysUser")
 public class SysUserController {
+    @Autowired
+    private SysUserService sysUserService;
 
 }
